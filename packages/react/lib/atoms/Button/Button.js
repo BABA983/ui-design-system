8270e7fe9a40dd876145eebd31b234f52885796d
BABA983 <692877906@qq.com>
2022-09-20 22:44:39 +0800

    feat: setup rollup

---
+++ packages/react/lib/atoms/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = (props) => {
+    const { children } = props;
+    return React.createElement("button", null, children);
+};
+
+export { Button as default };
+//# sourceMappingURL=Button.js.map
