1512b674c171ea1068a2981c9c8bafe87d02f4e9
BABA <692877906@qq.com>
2022-09-24 16:55:33 +0800

    feat: add Select component

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Color, Margin, Text } from '@ui-design-system/react';
+import { Button, Color, Margin, Select, Text } from '@ui-design-system/react';
 import '@ui-design-system/scss/lib/Button.css';
 import '@ui-design-system/scss/lib/Utilities.css';
 import '@ui-design-system/scss/lib/Text.css';
 import '@ui-design-system/scss/lib/Margin.css';
+import '@ui-design-system/scss/lib/Select.css';
+
+const options = [{
+    label: 'Strict Black',
+    value: 'strict-black'
+}, {
+    label: 'Heavenly Green',
+    value: 'heavenly-green'
+}, {
+    label: 'Sweet Pink',
+    value: 'pink'
+}]
 
 ReactDOM.render(
   <>
@@ -15,6 +27,8 @@ ReactDOM.render(
     <Margin left>
       <Text size="xs">Margin Text</Text>
     </Margin>
+	<Select options={options}/>
+	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima esse delectus pariatur repudiandae et rem repellat nostrum iure sunt quasi, iusto tenetur est, quia quo ipsam debitis quae nisi minus.</p>
   </>,
   document.getElementById('root')
 );
